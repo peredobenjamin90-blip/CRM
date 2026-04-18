32db5fa6a5e4ad75639ff37e8427b1f7b6847831
Benjamin Peredo <benjaminperedo@Benjamins-MacBook-Air.local>
2026-04-17 22:56:17 -0600

    feat: config multi-cliente con sheets por usuario

--- config.py
+++ config.py
@@ -1,13 +1,20 @@
 USUARIOS = {
-    "maxiclean": {
-        "password": "maxi2024",
-        "nombre": "Maxi Clean GDL",
-        "archivos": {
-            2022: "Maxi Clean 2022.xlsx",
-            2023: "Maxi Clean 2023.xlsx",
-            2024: "Maxi Clean 2024.xlsx",
-            2025: "Maxi Clean 2025.xlsx",
-            2026: "Maxi Clean 2026.xlsx"
+    "Maxiclean": {
+        "password": "maxiclean2024",
+        "nombre": "ChemDry Zapopan",
+        "sheets": {
+             2022: "1L3wzHhc6_sN7h361uqXFI7lKzGobl_vtuHEDVTbCEdc",
+    2023: "1e7B1Hp5zWJ3kLSS6d8CQoESYsaDWyIrFhIGcSRJE_Ag",
+    2024: "1cVfveU9err9N6RI23OOHEABV8m8Rj9wRkO3F27o6TAg",
+    2025: "1IodGW1K7c7GQBa90k6O8YCFOtwt3sA2PDFa7mmOkZ0E",
+    2026: "1mqcHNhQEjEhKYYuY6iDOVpmPDH7br0VJITxdVx7wzls",
+        }
+    },
+    "Polla": {
+        "password": "Pasteles12",
+        "nombre": "Pasteles Lupita",
+        "sheets": {
+            2026: "ID_SHEET_TIA"
         }
     }
 }
